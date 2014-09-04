0d53c362ab4115aceb7fb4138c1acc011712ee77
Cheng-i Wang <wangsix@gmail.com>
2014-09-03 18:33:20 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,11 @@
 from distutils.core import setup
 
 setup(name="vmo",
-      packages = ['vmo'],
+      packages = ['vmo','vmo.VMO'],
       version="0.1",
       description="vmo - Variable Markov Oracle in Python",
       author="Cheng-i Wang",
       author_email='chw160@ucsd.edu',
       url='https://bitbucket.org/ciwang/vmo',
-      py_modules = [
-                    'vmo',
-                    'vmo.analysis',
-                    'vmo.draw',
-                    'vmo.generate',
-                    'vmo.VMO',
-                    'vmo.VMO.oracle',
-                    ],
       long_description = 'vmo is a Python library for time series and symbolic sequence analysis/synthesis in the family of software built around the Factor Oracle and Variable Markov Oracle algorithms. One of the main innovations in vmo is using functions related to Information Dynamics to determine oracle structure and query-matching algorithms.',
       )
