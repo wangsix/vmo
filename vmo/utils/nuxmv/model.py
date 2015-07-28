9d5d6145a5d50592db38c06e7a5e38e6227f674d
tbazin <theis.bazin@ens-cachan.fr>
2015-07-28 14:13:19 -0700

    Removed use of bytearrays.

--- vmo/utils/nuxmv/model.py
+++ vmo/utils/nuxmv/model.py
@@ -45,25 +45,24 @@ def print_transitions(adj_lists, origin):
         origin_state: int
             The starting state in the graph
     ----
-    Two-states a & b, a leads to b and b leads to a
+    Two-states a and b, a leads to b and b leads to a.
     >>> adj_lists = [[1, 2], [0], [1]]
     >>> result = print_transitions(adj_lists, 0)
-    >>> expected = bytearray("{1, 2}")
+    >>> expected = "{1, 2}"
     >>> result == expected
     True
     """
     goals = sorted(adj_lists[origin])
 
-    to_bytearray = lambda state: bytearray(str(state))
-    goals_str = bytearray(", ").join(map(to_bytearray, goals))
+    goals_str = ", ".join([str(goal) for goal in goals])
 
     nuxmv_set = "{{{0}}}".format(goals_str)
     
-    return bytearray(nuxmv_set)
+    return nuxmv_set
 
 def print_state(adj_lists, s_index, nuxmv_state_name='s',
                 extended_guard=None):
-    """Return a nuXmv-formatted bytearray defining state s_index.
+    """Return a nuXmv-formatted string defining state s_index.
     
     Keyword arguments:
         adj_lists: list of lists of pairs
@@ -77,10 +76,10 @@ def print_state(adj_lists, s_index, nuxmv_state_name='s',
             A function to generate more complex guards
             (default None)
     ----
-    Two-states a & b, a leads to b and b leads to a
+    Two-states a and b, a leads to b and b leads to a.
     >>> adj_lists = [[1], [0]]
     >>> result = print_state(adj_lists, 0)
-    >>> expected = bytearray("s=0: {1};")
+    >>> expected = "s=0: {1};"
     >>> result == expected
     True
     """
@@ -90,13 +89,13 @@ def print_state(adj_lists, s_index, nuxmv_state_name='s',
         extended_guard_str = extended_guard(adj_lists, s_index,
                                             nuxmv_state_name) 
     
-    guard = bytearray("{0}={1}{2}: ".format(nuxmv_state_name, s_index,
-                                            extended_guard_str))
+    guard = "{0}={1}{2}: ".format(nuxmv_state_name, s_index,
+                                  extended_guard_str)
     transitions_str = print_transitions(adj_lists, s_index)
-    return _bytes_concat([guard, transitions_str, bytearray(';')])
+    return "".join([guard, transitions_str, ';'])
 
 def print_graph(adj_lists, init_state=0, nuxmv_state_name='s'):
-    """Return a list of nuXmv-formatted bytearrays defining all graph's states.
+    """Return a list of nuXmv-formatted strings defining all graph's states.
 
     Keyword arguments:
         adj_lists: list of lists of pairs
@@ -107,20 +106,18 @@ def print_graph(adj_lists, init_state=0, nuxmv_state_name='s'):
             The name of the state in the nuXmv model which is
             represented by the graph (default 's') 
     ----
-    Three-states (a, b and c), a leads to b or c, b and c lead to a
+    Three-states (a, b and c), a leads to b or c, b and c lead to a.
     >>> adj_lists = [[1, 2], [0], [0]]
     >>> result = print_graph(adj_lists, nuxmv_state_name='a')
-    >>> expected = (map(bytearray, [
-    ...      "VAR a: 0..2;",
-    ...      "ASSIGN init(a) := 0;"]
-    ...      ),
-    ...      map(bytearray, [
-    ...      "next(a) :=",
-    ...      "case",
-    ...      "a=0: {1, 2};",
-    ...      "a=1: {0};",
-    ...      "a=2: {0};",
-    ...      "esac;"])
+    >>> expected = (
+    ...      ["VAR a: 0..2;",
+    ...       "ASSIGN init(a) := 0;"],
+    ...      ["next(a) :=",
+    ...       "case",
+    ...       "a=0: {1, 2};",
+    ...       "a=1: {0};",
+    ...       "a=2: {0};",
+    ...       "esac;"])
     ...      )
     >>> result == expected
     True
@@ -131,27 +128,27 @@ def print_graph(adj_lists, init_state=0, nuxmv_state_name='s'):
     # Declare number of states
     range_decl = "VAR {0}: 0..{1};".format(nuxmv_state_name,
                                            states_num-1)
-    header.append(bytearray(range_decl))
+    header.append(range_decl)
     # Declare initial state
     init_state = "ASSIGN init({0}) := {1};".format(nuxmv_state_name,
                                                    init_state)
-    header.append(bytearray(init_state))
+    header.append(init_state)
     
     states = []
-    states.append(bytearray("next({}) :=".format(nuxmv_state_name)))
-    states.append(bytearray("case"))
+    states.append("next({}) :=".format(nuxmv_state_name))
+    states.append("case")
 
     for state in range(states_num):
-        new_state_bytes = print_state(adj_lists, state,
-                                      nuxmv_state_name=nuxmv_state_name)
-        states.append(new_state_bytes)
-    states.append(bytearray("esac;"))
+        new_state = print_state(adj_lists, state,
+                                nuxmv_state_name=nuxmv_state_name)
+        states.append(new_state)
+    states.append("esac;")
     
     return header, states
 
 def print_module(adj_lists, nuxmv_state_name='s', init_state=0,
                  module_name='main'):
-    """Return a nuXmv-formatted bytearray defining a dtmc-module for the graph.
+    """Return a nuXmv-formatted string defining a dtmc-module for the graph.
 
     Keyword arguments:
         adj_lists: list of list of pairs
@@ -164,7 +161,7 @@ def print_module(adj_lists, nuxmv_state_name='s', init_state=0,
     >>> adj_lists = [[1, 2], [0], [0]]
     >>> result = print_module(adj_lists, nuxmv_state_name='a',
     ...                       module_name='test')
-    >>> expected = bytearray(
+    >>> expected = (
     ...     "MODULE test()\\n" +
     ...     "VAR a: 0..2;\\n" +
     ...     "ASSIGN init(a) := 0;\\n" +
@@ -178,7 +175,7 @@ def print_module(adj_lists, nuxmv_state_name='s', init_state=0,
     >>> result == expected
     True
     """
-    header = bytearray("MODULE {0}()\n".format(module_name))
+    header = "MODULE {0}()\n".format(module_name)
     
     graph_header, graph_states = print_graph(adj_lists,
                                              init_state=init_state,
@@ -186,10 +183,10 @@ def print_module(adj_lists, nuxmv_state_name='s', init_state=0,
     graph_header_str = indent_join_lines(graph_header, tabulate_by=0)
     graph_states_str = indent_join_lines(graph_states) 
 
-    graph_str = _bytes_concat([graph_header_str,
-                               graph_states_str])
+    graph_str = "".join([graph_header_str,
+                         graph_states_str])
     
-    return _bytes_concat([header, graph_str])
+    return "".join([header, graph_str])
 
 def print_variable(name, values, initial_value, transitions):
     """Generic formatted variable declaration.
@@ -201,26 +198,21 @@ def print_variable(name, values, initial_value, transitions):
             The variable's type / range.
         initial_value: str
             The variable's initial value.
-        transitions: list of bytearrays
+        transitions: list of strings
             The list of all transitions for the variable.
     ----
     >>> name = 'var'
     >>> values = '{a, b}'
     >>> initial_value = 'a'
-    >>> transitions = map(bytearray,
-    ...                   ["var=a: b;",
-    ...                    "var=b: a;"])
-    >>> expected = (map(bytearray, [
-    ...      "VAR var : {a, b};",
-    ...      "ASSIGN init(var) := a;"]
-    ...      ),
-    ...      map(bytearray, [
-    ...      "next(var) :=",
-    ...      "case",
-    ...      "var=a: b;",
-    ...      "var=b: a;",
-    ...      "esac;"])
-    ...      )
+    >>> transitions = ["var=a: b;",
+    ...                "var=b: a;"]
+    >>> expected = (["VAR var : {a, b};",
+    ...              "ASSIGN init(var) := a;"],
+    ...             ["next(var) :=",
+    ...              "case",
+    ...              "var=a: b;",
+    ...              "var=b: a;",
+    ...              "esac;"])
     >>> result = print_variable(name, values, initial_value,
     ...                         transitions)
     >>> result == expected
@@ -228,15 +220,15 @@ def print_variable(name, values, initial_value, transitions):
     """
     header = []
     variable_decl = ("VAR {0} : {1};".format(name, values))
-    header.append(bytearray(variable_decl))
+    header.append(variable_decl)
     variable_init = "ASSIGN init({0}) := {1};".format(name, initial_value)
-    header.append(bytearray(variable_init))
+    header.append(variable_init)
     
     cases = []
-    cases.append(bytearray("next({0}) :=".format(name)))
-    cases.append(bytearray("case"))
+    cases.append("next({0}) :=".format(name))
+    cases.append("case")
     cases += transitions
-    cases.append(bytearray("esac;"))
+    cases.append("esac;")
 
     return header, cases
 
@@ -260,15 +252,15 @@ def print_pitch_state(oracle, state, nuxmv_state_name='s'):
     >>> o = vmo.VMO.oracle.build_oracle(chromagram.T, 'a',
     ...                                 feature='chromagram')
     >>> result = print_pitch_state(o, 1)
-    >>> expected = bytearray("next(s)=1: p_C;")
+    >>> expected = "next(s)=1: p_C;"
     >>> result == expected
     True
     """
     chroma_array = oracle.feature[state]
     chord = chroma.to_chord(chroma_array)
     root_name = make_root_name(chord)
-    return bytearray("next({0})={1}: {2};".format(nuxmv_state_name, state,
-                                                   root_name))
+    return "next({0})={1}: {2};".format(nuxmv_state_name, state,
+                                        root_name)
 
 def print_pitches(oracle, nuxmv_state_name='s'):
     """Return full nuXmv variable declaration for pitch in `oracle`.
@@ -285,21 +277,18 @@ def print_pitches(oracle, nuxmv_state_name='s'):
     >>> s.append(music21.chord.Chord([], quarterLength=1))  # silent frame
     >>> oracle = vmusic.from_stream(s)
     >>> result = print_pitches(oracle)
-    >>> expected = (map(bytearray, [
-    ...                 "VAR pitchRoot : " +
-    ...                 "{p_Init, p_Silence, p_C, p_C#, p_D, p_E-, p_E, " +
-    ...                 "p_F, p_F#, p_G, p_G#, p_A, p_B-, p_B};",
-    ...                 "ASSIGN init(pitchRoot) := p_Init;"]),
-    ...             map(bytearray, [
-    ...                 "next(pitchRoot) :=",
-    ...                 "case",
-    ...                 "next(s)=0: p_Init;",
-    ...                 "next(s)=1: p_C;",
-    ...                 "next(s)=2: p_E;",
-    ...                 "next(s)=3: p_C;",
-    ...                 "next(s)=4: p_Silence;",
-    ...                 "esac;"])
-    ...                 )
+    >>> expected = (["VAR pitchRoot : " +
+    ...              "{p_Init, p_Silence, p_C, p_C#, p_D, p_E-, p_E, " +
+    ...              "p_F, p_F#, p_G, p_G#, p_A, p_B-, p_B};",
+    ...              "ASSIGN init(pitchRoot) := p_Init;"],
+    ...             ["next(pitchRoot) :=",
+    ...              "case",
+    ...              "next(s)=0: p_Init;",
+    ...              "next(s)=1: p_C;",
+    ...              "next(s)=2: p_E;",
+    ...              "next(s)=3: p_C;",
+    ...              "next(s)=4: p_Silence;",
+    ...              "esac;"])
     >>> result == expected
     True
     """
@@ -310,7 +299,7 @@ def print_pitches(oracle, nuxmv_state_name='s'):
     
     transitions = []
     transitions.append(
-        bytearray("next({})=0: p_Init;".format(nuxmv_state_name)))
+        "next({})=0: p_Init;".format(nuxmv_state_name))
     for s in range(1, oracle.n_states):
         transitions.append(print_pitch_state(oracle, s, nuxmv_state_name))
 
@@ -340,13 +329,11 @@ def print_pitchspace_state(oracle, original_stream, state, nuxmv_state_name='s')
     >>> s.append(copy.deepcopy(c1))
     >>> o = vmusic.from_stream(s, threshold=0.01)
     >>> result = print_pitchspace_state(o, s, 1)
-    >>> expected = bytearray("next(s)=1: {};".format(
-    ...                        c1.root().midi))
+    >>> expected = "next(s)=1: {};".format(c1.root().midi)
     >>> result == expected
     True
     >>> result = print_pitchspace_state(o, s, 2)
-    >>> expected = bytearray("next(s)=2: {};".format(
-    ...                        c2.root().midi))
+    >>> expected = "next(s)=2: {};".format(c2.root().midi)
     >>> expected == result 
     True
     """
@@ -363,12 +350,10 @@ def print_pitchspace_state(oracle, original_stream, state, nuxmv_state_name='s')
         return chord.root().midi if chord.pitches else -1
 
     transition = "next({0})={1}: {2};".format(nuxmv_state_name,
-                                             state,
-                                             get_ps_root_of_frame(state))
+                                              state,
+                                              get_ps_root_of_frame(state))
 
-    return bytearray(transition)
-
-    return transitions
+    return transition
 
 def print_pitchspaces(oracle, original_stream, nuxmv_state_name='s',
                       nuxmv_pitchspace_name='pitchSpace'):
@@ -401,9 +386,9 @@ def print_motions_generic_define(nuxmv_motion_name='pitchMotion',
                                  nuxmv_pitchspace_name='pitchSpace'):
     """Return the generic variable declaration for melodic motion.
     
-    The values of this variable depend only on the pitch space variable.
+    The nuXmv values of this variable depend only on the pitch space variable.
     """                             
-    output = bytearray(
+    output = (
         "VAR {0} : {{m_none, m_desc, m_asc}};\n".format(nuxmv_motion_name) +
         "ASSIGN init({}) := m_none;\n".format(nuxmv_motion_name) +
         "\tnext({}) :=\n".format(nuxmv_motion_name) +
@@ -421,7 +406,7 @@ def print_oracle(oracle, enable_motions=False, include_rsfx=False,
                  init_state=None, nuxmv_state_name='s', original_stream=None,
                  nuxmv_motion_name='pitchMotion',
                  nuxmv_pitchspace_name='pitchSpace'):
-    """Return a bytearray describing `oracle`, with oracle states and pitches.
+    """Return a string describing `oracle`, with oracle states and pitches.
 
     Assumes the oracle has been created with a chromagram as feature.
     ----
@@ -435,7 +420,7 @@ def print_oracle(oracle, enable_motions=False, include_rsfx=False,
     >>> o = vmo.VMO.oracle.build_oracle(chromagram.T, 'a',
     ...                                 feature='chromagram')
     >>> result = print_oracle(o, enable_motions=False)
-    >>> expected = bytearray(
+    >>> expected = (
     ...     "MODULE main()\\n" +
     ...     "VAR s: 0..2;\\n" +
     ...     "ASSIGN init(s) := 0;\\n" +
@@ -453,8 +438,9 @@ def print_oracle(oracle, enable_motions=False, include_rsfx=False,
     ...     "\\tnext(s)=0: p_Init;\\n" +
     ...     "\\tnext(s)=1: p_C;\\n" +
     ...     "\\tnext(s)=2: p_E;\\n" +
-    ...     "\\tesac;\\n"
-    ...     )
+    ...     "\\tesac;\\n\\n" +
+    ...     "-- Motions not enabled."
+    ...    )
     >>> result == expected
     True
     """
@@ -474,7 +460,7 @@ def print_oracle(oracle, enable_motions=False, include_rsfx=False,
         header_str = indent_join_lines(header, tabulate_by=0)
         cases_str = indent_join_lines(cases)
         
-        var_str = _bytes_concat([header_str, cases_str])
+        var_str = "".join([header_str, cases_str])
         return var_str
 
     pitches_str = full_variable_printer(print_pitches(oracle, nuxmv_state_name))
@@ -488,10 +474,9 @@ def print_oracle(oracle, enable_motions=False, include_rsfx=False,
                 nuxmv_pitchspace_name=nuxmv_pitchspace_name,
                 nuxmv_motion_name=nuxmv_motion_name)
     else:
-        motions_str = ""
+        motions_str = "-- Motions not enabled."
         
-    return _bytes_concat([base_model, bytearray("\n"),
-                          pitches_str, motions_str])
+    return "\n".join([base_model, pitches_str, motions_str])
 
         
 """Auxiliary functions"""
@@ -510,20 +495,7 @@ def indent_join_lines(lines, tabulate_by=1, trailing='\n'):
     tabulation = "\t" * tabulate_by
     tabulate_line = lambda line : tabulation + line + trailing
     tabbed_lines = map(tabulate_line, lines)
-    return _bytes_concat(tabbed_lines)
-
-def _bytes_concat(bytearrays):
-    """Efficiently concatenate the input bytearrays.
-
-    Keyword arguments:
-        bytearrays: list of byetarray
-            The bytearrays to concatenate
-    ----
-    >>> bytes = bytearray("Hello !")
-    >>> _bytes_concat([bytes]*2) == bytearray("Hello !Hello !")
-    True
-    """ 
-    return bytearray("").join(bytearrays)    
+    return "".join(tabbed_lines)
 
 def make_root_name(chord, nuxmv_silence_name='p_Silence'):
     """Generic function to return a valid root name for a model.
