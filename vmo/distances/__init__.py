Fixed a bug in Tonnetz distance (wrong normalization) + added normalization in cdist

vmo/distances/__init__.py
@@ -20,29 +20,43 @@ You should have received a copy of the GNU General Public License
 along with vmo.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+import numpy as np
+
 import scipy.spatial.distance as scidist
+import sklearn.preprocessing as preproc
 import vmo.distances.tonnetz
 
-def cdist(XA, XB, dfunc='euclidean', **kwargs):
-    """Compute distance between each pair of the two collections of inputs.
+def cdist(XA, XB, dfunc='euclidean', normalize=True, **kwargs):
+    """Compute distances between each pair of the two collections of inputs.
 
     Overloads the same-named function from scipy with self-defined metrics.
+    The arguments are normalized by default.
+
+    Added keyword arguments:
+        normalize: bool, optional
+           Whether the input arrays should be column-wise normalized
+           (default True).
     
     Keyword arguments: (taken from the SciPy doc)
-        XA: numpy.ndarray
+        XA : numpy.ndarray
             An mA by n array of mA original observations in an
             n-dimensional space. Inputs are converted to float type.
-        XB : ndarray
+        XB : numpy.ndarray
             An mB by n array of mB original observations in an
             n-dimensional space. Inputs are converted to float type.
-        metric : str or callable, optional
+        dfunc : str or callable, optional
+            (Called `metric` within scipy.)
             The distance metric to use.
             See list of scipy supported metrics in the associated doc.
             If callable, must provide a function of arity 2, e.g.:
-            <lambda u, v: np.sqrt(((u-v)**2).sum()>, for euclidean distance
+            <lambda u, v: np.sqrt(((u-v)**2).sum()>, for euclidean distance.
     
     See specification for scipy.spatial.distance.cdist for further details.
     """
+    if normalize:
+        XA = preproc.normalize(np.array(XA).astype(float))
+        XB = preproc.normalize(np.array(XB).astype(float))
+    
     if dfunc == 'tonnetz':
         return scidist.cdist(XA, XB, metric=tonnetz.distance, **kwargs)
     else:

vmo/distances/tonnetz.py
@@ -75,18 +75,19 @@ def _make_tonnetz_matrix():
 __TONNETZ_MATRIX = _make_tonnetz_matrix()
 
 def _to_tonnetz(chromagram):
-    """Project a chromagram on the tonnetz
+    """Project a chromagram on the tonnetz.
 
-    Return value is normalized to prevent numerical instabilities  
+    Returned value is normalized to prevent numerical instabilities.  
     """
+    if np.sum(np.abs(chromagram)) == 0.:
+        # The input is an empty chord, return zero. 
+        return np.zeros(6)
+    
     _tonnetz = np.dot(__TONNETZ_MATRIX, chromagram)
-    one_norm = np.sum(np.abs(chromagram))
-    if (one_norm != 0.):
-        _tonnetz = _tonnetz / one_norm # Normalize tonnetz vector
-    else:
-        _tonnetz = np.zeros(6) # Norm is zero, nullify vector
+    one_norm = np.sum(np.abs(_tonnetz))  # Non-zero value
+    _tonnetz = _tonnetz / float(one_norm) # Normalize tonnetz vector
     return _tonnetz
-
+    
 def distance(a, b):
     """Compute tonnetz-distance between two chromagrams.
     