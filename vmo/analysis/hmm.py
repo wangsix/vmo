012b0b4250afcd5d49529e8adbd0bec409dbf49e
Cheng-i Wang <wangsix@gmail.com>
2017-03-07 18:00:32 -0800

    fixed recognization function

--- vmo/analysis/hmm.py
+++ vmo/analysis/hmm.py
@@ -34,20 +34,21 @@ def extract_hmm_tensor(oracle, max_lrs=None, normalize=True, smooth=False):
 
     for r, d, e in zip(oracle.lrs[:1:-1], oracle.data[:1:-1], oracle.data[-2:0:-1]):
         if r <= 2:
-            hmm_tensor[:1, d, e] += 1.0
+            hmm_tensor[:1, e, d] += 1.0
         else:
-            hmm_tensor[:r - 1, d, e] += 1.0
+            hmm_tensor[:r - 1, e, d] += 1.0
 
     if normalize:
         for i, m in enumerate(hmm_tensor):
-            divider = np.sum(m, axis=0)
+            # m += np.finfo('float').eps
+            divider = np.sum(m, axis=1)
             divider[divider == 0.0] = 1.0
-            hmm_tensor[i] = np.divide(m, divider).T
+            hmm_tensor[i] = np.divide(m.T, divider).T
 
     return hmm_tensor
 
 
-def inference_latent(obs, oracle, order=1, smooth=False):
+def recognition(obs, oracle, order=1, smooth=False):
     hmm_tensor = extract_hmm_tensor(oracle, max_lrs=order, smooth=smooth)
 
     cluster_means = np.array([np.median(oracle.f_array.data[np.array(c), :].T, axis=1)
@@ -57,18 +58,21 @@ def inference_latent(obs, oracle, order=1, smooth=False):
     cluster_means = (cluster_means.T / np.sum(cluster_means, axis=1)).T
 
     a = hmm_tensor[-1]
-    a += np.finfo('float').eps
-    divider = np.sum(a, axis=0)
-    a = np.divide(a, divider).T
+    # a += np.finfo('float').eps
+    a += 1.0
+    divider = np.sum(a, axis=1)
+    a = np.divide(a.T, divider).T
     log_a = np.log(a)
+    hist = np.array([len(c) for c in oracle.latent])/float(oracle.n_states-1)
 
     v = np.zeros((len(obs), len(oracle.latent)))
     p = np.zeros(v.shape)
     v[0] = np.log(np.dot(cluster_means, obs[0])) + np.log(1.0/len(oracle.latent))
-    p[0] = np.arange(len(oracle.latent))
+    # v[0] = np.log(np.dot(cluster_means, obs[0])) + np.log(hist)
+    # p[0] = np.arange(len(oracle.latent))
     for t in range(1, len(obs)):
-        s = v[t-1]+log_a+np.log(np.dot(cluster_means, obs[t]))
-        p[t] = np.argmax(s, axis=1)
-        v[t] = s[range(0, len(oracle.latent)), p[t].astype('int')]
+        s = v[t-1]+log_a.T
+        v[t] = np.max(s, axis=1)+np.log(np.dot(cluster_means, obs[t]))
+        p[t-1] = np.argmax(s, axis=1)
 
     return v, p
